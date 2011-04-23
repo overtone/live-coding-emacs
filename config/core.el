106d2ae865d656adf15daa0d1a59c76a56a589b9
Sam Aaron <samaaron@gmail.com>
2011-04-23 15:40:33 +0100

    Add missing file extension - Thanks to Iain Wood for spotting this

--- config/core.el
+++ config/core.el
@@ -2,4 +2,4 @@
 ;; Either place config straight in here, or load external files such as this bindings file:
 
 ;; Load personal bindings
-(load-dotfile "config/bindings")
+(load-dotfile "config/bindings.el")
