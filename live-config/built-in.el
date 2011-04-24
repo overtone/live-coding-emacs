d72aa4c42f7b1be173518d62ee64bd047da631e2
Sam Aaron <samaaron@gmail.com>
2011-04-24 11:59:12 +0100

    create autosaves and backups tmp dirs if necessary

--- live-config/built-in.el
+++ live-config/built-in.el
@@ -2,11 +2,14 @@
 (require 'ansi-color)
 (require 'recentf)
 
+;; create autosaves and backups tmp dirs if necessary
+(make-directory (concat dotfiles-tmp-dir "autosaves") t)
+(make-directory (concat dotfiles-tmp-dir "backups") t)
+
 ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/tmp/
 (setq auto-save-file-name-transforms `((".*" ,(concat dotfiles-tmp-dir "autosaves/\\1") t)))
 (setq backup-directory-alist `((".*" . ,(concat dotfiles-tmp-dir "backups"))))
 (setq auto-save-list-file-name (concat dotfiles-tmp-dir "autosaves/autosave-list"))
-
 
 ;;When you visit a file, point goes to the last place where it was when you previously visited
 ;;Save file is set to dotfiles-tmp-dir/places
