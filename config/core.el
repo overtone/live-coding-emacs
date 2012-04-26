159fe866ceabdb1846c4cc74e1397a0e421829f9
Daniel Jomphe <danieljomphe@gmail.com>
2012-04-26 06:57:14 -0400

    Add support for *personal* ELPA packages (in config/core.el). That's where people should install their favorite packages.

--- config/core.el
+++ config/core.el
@@ -1,6 +1,20 @@
 ;; Here is the root of your personal configs.
 ;; Either place config straight in here,
 ;; such as this colour theme (feel free to change it to your own favourite theme)
+;;; --------------------------------------------------------------------------
+;;; PACKAGES
+;;; --------------------------------------------------------------------------
+(defvar my-packages
+  '(
+  ;;color-theme-sanityinc-tomorrow
+  ;;color-theme-sanityinc-solarized
+  ;;starter-kit
+  ;;starter-kit-bindings
+    )
+  "A list of packages to ensure are installed at launch.")
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
 ;; use blackbored colour theme
 (load-file (concat dotfiles-lib-dir "blackbored.el"))
